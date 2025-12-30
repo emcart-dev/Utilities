e083c021cdfc6bf1061804e7a9328656d6cda09d
Emily Cartwright <emilycharlottecartwright@gmail.com>
2025-12-31 03:03:17 +1100

    Updating all files

--- samples/Utilities.Demo/Program.cs
+++ samples/Utilities.Demo/Program.cs
@@ -4,10 +4,10 @@ using Utilities.Strings;
 class Program {
     static void Main(string[] args) {
         //  Transforming Strings  |  Reverse String
-        Console.WriteLine(StringTransform.Reverse("hello"));
+        StringTransform.Reverse("hello");
         
         //  Transforming Strings  |  Convert String to Title Case
-        Console.WriteLine(StringTransform.ToTitleCase("Epiphany"));
+        StringTransform.ToTitleCase("Epiphany");
     }
 }
 
