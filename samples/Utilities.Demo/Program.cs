aef9553657f881b4d7855547b0823e67aa549bdf
Emily Cartwright <emilycharlottecartwright@gmail.com>
2026-01-02 02:14:25 +1100

    Updates to String Sanitizer program

--- samples/Utilities.Demo/Program.cs
+++ samples/Utilities.Demo/Program.cs
@@ -1,14 +1,15 @@
 //  Demo console for testing classes and calling methods
+using System;
 using Utilities.Strings;
 
 class Program {
     static void Main(string[] args) {
-        //  Transforming Strings  |  Reverse String
-        StringTransform.Reverse("hello");
-        
-        //  Transforming Strings  |  Convert String to Title Case
-        StringTransform.ToTitleCase("Epiphany");
+            /*  Transforming Strings  |  Reverse String
+            StringTransform.Reverse("hello");
+            //  Transforming Strings  |  Convert String to Title Case
+            StringTransform.ToTitleCase("Epiphany"); */
+            //  Sanitizing Strings  |  Convert String Into A Single Space String
+            Console.WriteLine(StringSanitizer.CollapseWhiteSpace("   \n\n Hi There, I'm Emmy!   \t\t\t\t"));
     }
 }
-
 
